6864887b8285b7ec5800ad1571a56e406fff12cf
William <patwill1242@gmail.com>
2026-04-15 10:50:46 +0200

    dictionary exercises

---
+++ glossary.py
@@ -0,0 +1,22 @@
+glossary = {
+    'variable':'A named container that stores a value, like a labeled box.',
+    'function':'A reusable block of code that performs a specific task.',
+    'loop':'A instruction that repeats a block of code multiple times.',
+    'bug':'An error or flaw in the code that causes it to behave unexpectedly.',
+    'string':'A sequence of characters — basically text.'
+}
+
+print("Variable:")
+print(f"\t{glossary['variable']}\n")
+
+print("Function:")
+print(f"\t{glossary['function']}\n")
+
+print("Loop:")
+print(f"\t{glossary['loop']}\n")
+
+print("Bug:")
+print(f"\t{glossary['bug']}\n")
+
+print("String:")
+print(f"\t{glossary['string']}\n")
---
+++ person.py
@@ -0,0 +1,8 @@
+person = {
+    'first_name':'Jordan',
+    'last_name':'Soller',
+    'age':'22',
+    'city':'Milan'
+}
+
+print(person)
